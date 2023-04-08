472b7913a25abd3a3ca83fee15d8a30fa58047cb
xDp64xG <brow.paul64@gmail.com>
2023-04-08 18:13:59 -0400

    Updated gui and readme
    Using .grid instead of .pack for widgets...seems to hold. Need to understand more of widget placement

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # DDS Bag Calculator
  Calculate exact bag accounts in DDS inputted into 'numbers.txt' file by the user and outputted to 'finished.txt'.
 
+# Version and Update Log
+ 0.2 - Trying to use a new function for the program, to try and fine tune whats on the window and where at. Planning to expand upon this.
+
+ 0.1 - GUI version was born...take a look at one of the old images at the bottom...looks ugly but works. This was created with a personal focus of fulfilling orders on DDS, and thought about trying to make it universal for anyone to use.
+
 # How to Run?
  Install Python, currently works for 3.8, run 'pip install py2exe', and then in the directory of main.py and setup.py, run 'python setup.py py2exe' in the command prompt. After finished, check dist folder, and run main.exe, everything should be included.
 
@@ -15,10 +20,13 @@
 
 # Images of GUI DDS
 <div style ="width:60px ; height:60px">
-<img src = 'https://snipboard.io/lgQHbS.jpg' alt="Intro of Program" width="25%" height="25%">
-<img src = 'https://snipboard.io/MrQiUT.jpg' alt="Update Program" width="25%" height="25%">
+<img src = 'https://snipboard.io/yRgnrH.jpg' alt="Intro of Program" width="25%" height="25%">
+<img src = 'https://snipboard.io/5wYRr8.jpg' alt="Update Program" width="25%" height="25%">
 <img src = 'https://snipboard.io/Xm47ih.jpg' alt="New data" width="25%" height="25%">
+
 </div>
 
+# Got your own ideas?
+Reach out to me via github, let me know what you think would work.
 
 Original Code found here: https://github.com/xDp64xG/DDS-Custom-Bag-Calculator
--- main.py
+++ main.py
@@ -10,45 +10,47 @@ class DDS:
         self.array = ""
         self.lines2 = ""
         self.window = tk.Tk()
+        self.window.title("DDS Bag Calculator")
+        self.window.config(bg="lightgrey")
+        # self.window.attributes('-fullscreen', True)
+        self.window.resizable(True, True)
 
+        # Our Frame
+        self.content = tk.Frame(self.window, borderwidth=5, relief='ridge')
+
+        self.window.grid_rowconfigure(1, weight=1)
+        self.window.grid_columnconfigure(0, weight=1)
+
+        self.content.grid(column=0, row=0, columnspan=5, rowspan=5, sticky=(tk.N + tk.S + tk.E + tk.W))
+
+        # Our scrollbar
         v = tk.Scrollbar(self.window, orient='vertical')
-        v.pack(side=tk.RIGHT, fill='y')
-        self.frame_a = tk.Frame(master=self.window, width=75, height=75)
-        self.frame_b = tk.Frame(master=self.window, width=50, height=50, bg='black')
-        self.frame_c = tk.Frame(master=self.window, width=100, height=100, bg='red')
+        v.grid(column=5, row=1, rowspan=2)
 
-        self.label = tk.Label(master=self.frame_b,
-                              height=75,
-                              width=75,
-                              bg="black",
-                              fg="white",
-                              font=("Times New Roman", 15),
-                              )
-        self.t = tk.Text(self.label, yscrollcommand=v.set)
-        self.t.pack(expand=True, fill=tk.BOTH)
-        self.t.config(width=50,
-                      height=50,
-                      bg='black',
-                      fg='white',
-                      font=("Times New Roman", 15))
-        #v.config(command=self.label.yview)
-
-        self.entry = tk.Text(master=self.frame_a,
+        # Self t is output
+        self.t = tk.Text(master=self.content,
+                         bg='black',
+                         fg='white',
+                         height=50,
+                         width=50,
+                         state='disabled',
+                         yscrollcommand=v.set)
+        self.t.grid(column=4, row=1, rowspan=2, columnspan=6, sticky=(tk.E))
+        self.entry = tk.Text(master=self.content,
                              width=50,
                              height=50,
                              fg='black',
-                             bg='white')
+                             bg='white',
+                             font=('Times New Roman', 11))
         v.config(command=self.t.yview)
-        # self.numbersR = open('numbers.txt', 'r')
-        # self.numbersW = open('numbers.txt', 'w')
-        # self.finishedW = open('finished.txt', 'w')
+        self.entry.grid(column=0, row=1, columnspan=1, rowspan=1, sticky=(tk.W))
 
     def main(self):
 
         array = ""
 
         def process():
-            #If text widget has data, delete and start fresh
+            # If text widget has data, delete and start fresh
             try:
                 self.t.delete("1.0", tk.END)
             except:
@@ -84,132 +86,83 @@ class DDS:
             Lines = file1.readlines()
             lines2 = ""
 
-            # Cycle through text file
-
             # Switch print to writing to file
             def write(var):
                 file2 = open('finished.txt', 'a')
                 file2.writelines(var)
                 file2.close()
 
+            # Cycle through text file
             for i in Lines:
-                # print('i: {}'.format(i))
                 lines2 += i
-                # print(i)
                 try:
-
                     var = int(i)
                     bool = False
                 except:
                     var2 = str(i)
                     var = 0
-                    # print("--------------------\n{}".format(var2))
                     line += "--------------------\n{}".format(var2)
-                    # write(line)
                     bool = True
 
                 if bool == True:
                     pass
                 else:
                     # Cycle through all current bag sizes
-                    if var < Kilo and var > Half_K or var == Kilo:
+                    if Kilo > var > Half_K or var == Kilo:
                         Bag = var - Kilo
                         if Bag == 0:
-                            # print("[{}]: Use a {} gram bag\n".format(var, Kilo))
                             line += "[{}]: Use a {} gram bag\n".format(var, Kilo)
-                            # write(line)
                         else:
-                            # print("[{}]: Use a {} gram bag\nThen put up {}\n".format(var, Kilo, Bag))
                             line += "[{}]: Use a {} gram bag\nThen put up {}\n".format(var, Kilo, Bag)
-                            # write(line)
-
-                    elif var < Half_K and var > Hundred or var == Half_K:
+                    elif Half_K > var > Hundred or var == Half_K:
                         Bag = var - Half_K
                         if Bag == 0:
-                            # print("[{}]: Use a {} gram bag\n".format(var, Half_K))
                             line += "[{}]: Use a {} gram bag\n".format(var, Half_K)
-                            # write(line)
                         else:
-                            # print("[{}]: Use a {} gram bag\nThen put up {}\n".format(var, Half_K, Bag))
                             line += "[{}]: Use a {} gram bag\nThen put up {}\n".format(var, Half_K, Bag)
-                            # write(line)
-
-                    elif var < Hundred and var > Fifty or var == Hundred:
+                    elif Hundred > var > Fifty or var == Hundred:
                         Bag = var - Hundred
                         if Bag == 0:
-                            # print("[{}]: Use a {} gram bag\n".format(var, Hundred))
                             line += "[{}]: Use a {} gram bag\n".format(var, Hundred)
-                            # write(line)
                         else:
-                            # print("[{}]: Use a {} gram bag\nThen put up {}\n".format(var, Hundred, Bag))
                             line += "[{}]: Use a {} gram bag\nThen put up {}\n".format(var, Hundred, Bag)
-                            # write(line)
 
-                    elif var < Fifty and var > Twenty or var == Fifty:
+                    elif Fifty > var > Twenty or var == Fifty:
                         Bag = var - Fifty
                         if Bag == 0:
-                            # print("[{}]: Use a {} gram bag\n".format(var, Fifty))
                             line += "[{}]: Use a {} gram bag\n".format(var, Fifty)
-                            # write(line)
                         else:
-                            # print("[{}]: Use a {} gram bag\nThen put up {}\n".format(var, Fifty, Bag))
                             line += "[{}]: Use a {} gram bag\nThen put up {}\n".format(var, Fifty, Bag)
-                            # write(line)
-
-                    elif var < Twenty and var > Ten or var == Twenty:
+                    elif Twenty > var > Ten or var == Twenty:
                         Bag = var - Twenty
                         if Bag == 0:
-                            # print("[{}]: Use a {} gram bag\n".format(var, Twenty))
                             line += "[{}]: Use a {} gram bag\n".format(var, Twenty)
-                            # write(line)
                         else:
-                            # print("[{}]: Use a {} gram bag\nThen put up {}\n".format(var, Twenty, Bag))
                             line += "[{}]: Use a {} gram bag\nThen put up {}\n".format(var, Twenty, Bag)
-                            # write(line)
-
-                    elif var < Ten and var > Five or var == Ten:
+                    elif Ten > var > Five or var == Ten:
                         Bag = var - Ten
                         if Bag == 0:
-                            # print("[{}]: Use a {} gram bag\n".format(var, Ten))
                             line += "[{}]: Use a {} gram bag\n".format(var, Ten)
-                            # write(line)
                         else:
-                            # print("[{}]: Use a {} gram bag\nThen put up {}\n".format(var, Ten, Bag))
                             line += "[{}]: Use a {} gram bag\nThen put up {}\n".format(var, Ten, Bag)
-                            # write(line)
-
-                    elif var <= Five and var > Three:
+                    elif Five >= var > Three:
                         Bag = var - Five
                         if Bag == 0:
-                            # print("[{}]: Use a {} gram bag\n".format(var, Five))
                             line += "[{}]: Use a {} gram bag\n".format(var, Five)
-                            # write(line)
                         else:
-                            # print("[{}]: Use a {} gram bag\nThen put up {}\n".format(var, Five, Bag))
                             line += "[{}]: Use a {} gram bag\nThen put up {}\n".format(var, Five, Bag)
-                            # write(line)
-
                     else:
                         if var == 3:
-                            # print("[{}]: Use a 3 gram bag\n".format(var))
                             line += "[{}]: Use a 3 gram bag\n".format(var)
-                            # write(line)
-
                         elif var == 2:
-                            # print("[{}]: Use a 2 gram bag\n".format(var))
                             line += "[{}]: Use a 2 gram bag\n".format(var)
-                            # write(line)
-
                         else:
                             if var == 1:
-                                # print("[{}]: Use a 1 gram bag\n".format(var))
                                 line += "[{}]: Use a 1 gram bag\n".format(var)
-                                # write(line)
                             else:
                                 pass
             file1.close()
-            print("Finished. Check 'finished.txt'")
-            # print(line)
+            # print("Finished. Check 'finished.txt'")
 
             write(line)
             self.lines2 = lines2
@@ -225,15 +178,14 @@ class DDS:
             file1.writelines(data)
             file1.close()
             var = process()
+            self.t.config(state='normal')
             try:
-                #self.label.config(text=var)
                 self.t.insert("1.0", var)
-                #self.t.config(text=var)
-                #self.t.pack(master=self.frame_b)
             except:
                 print("Unable to config label")
 
-            self.label.pack()
+            self.t.grid(column=4, row=1, rowspan=2, columnspan=6, sticky=(tk.E))
+            self.t.config(state='disabled')
 
         def on_exit():
             print("Exiting...")
@@ -243,16 +195,19 @@ class DDS:
         line = process()
         array = ""
         array = line
-
-        window = self.window
         label = tk.Label(
-            master=self.frame_a,
+            master=self.content,
             text="Welcome to DDS Bag Calculator.\nUse the template and put \n"
                  "in your order numbers corresponding\n"
                  " to each category and hit refresh on the far right!")
 
-        button2 = tk.Button(
-            master=self.frame_c,
+        output = tk.Label(master=self.content,
+                          text="Fill in your order on the left and hit\n"
+                               " the refresh button. Below shows how\n"
+                               " to get each order")
+
+        refresh = tk.Button(
+            master=self.content,
             text="Refresh Data",
             width=15,
             height=5,
@@ -261,8 +216,8 @@ class DDS:
             command=handle_click2
         )
 
-        button3 = tk.Button(
-            master=self.frame_c,
+        exit = tk.Button(
+            master=self.content,
             text="Exit",
             width=15,
             height=5,
@@ -270,24 +225,16 @@ class DDS:
             fg='red',
             command=on_exit
         )
-        entry = self.entry
+
         template = 'Amp/Fet\n13\nGanja/Grass\n19\nEctasy/Candy\n23\nMeth\n41\nCoke\n11\nHero\n9\n4'
-        # entry = tk.Text()
-        entry.insert(tk.END, template)
+        self.entry.insert(tk.END, template)
 
-        label.pack()
-        entry.pack()
+        label.grid(column=0, row=0, sticky=tk.NW)
+        refresh.grid(column=1, row=4, pady=5, padx=5)
+        exit.grid(column=2, row=4, pady=5, padx=5)
+        output.grid(column=4, row=0, sticky=tk.NE)
+        self.window.mainloop()
 
-        button2.pack()
-        button3.pack()
-        # label = tk.Label(text=array)
-        # label.pack()
-        self.frame_a.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
-        self.frame_b.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
-        self.frame_c.pack(fill=tk.BOTH, side=tk.LEFT, expand=True)
-        window.mainloop()
 
 loop = DDS()
 loop.main()
-
-# foundation
